9b494bf1fd5227b4462e05cc9bf72673cb9e71ed
Walidnurudin <walidnurudin47@gmail.com>
2021-09-07 21:13:41 +0700

    task_1

---
+++ part_1/task_1.js
@@ -0,0 +1,43 @@
+const biodata = {
+    name: "Walid nurudin",
+    age: 20,
+    hobbies: ["main game", "futsal", "mendengarkan musik"],
+    IsMaried: false,
+    schoolList: [
+        {
+            name: "SDN SALAMDARMA 1",
+            yearIn: 2006,
+            yearOut: 2012,
+            major: null
+        },
+        {
+            name: "MTsN 5 INDRAMAYU",
+            yearIn: 2012,
+            yearOut: 2015,
+            major: null
+        },
+        {
+            name: "SMK AL-HUDA kedungwungu",
+            yearIn: 2015,
+            yearOut: 2018,
+            major: "Multimedia"
+        },
+    ],
+    skills: [
+        {
+            skillName: "Javascript",
+            level: "advanced"
+        },
+        {
+            skillName: "react",
+            level: "advanced"
+        },
+        {
+            skillName: "express",
+            level: "beginner"
+        }
+    ],
+    interestInCoding: true
+}
+
+console.log(biodata)
